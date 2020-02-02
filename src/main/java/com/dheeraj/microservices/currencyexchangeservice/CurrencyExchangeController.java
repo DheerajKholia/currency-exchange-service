4360515e72bd6eb2e9a8bc9cde8b3d6eba4f2606
DheerajKholia <dheraj.kholia@yahoo.com>
2020-02-02 12:03:41 +0530

    Hardcoded version of currency exchange service

--- src/main/java/com/dheeraj/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/dheeraj/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,4 +1,16 @@
 package com.dheeraj.microservices.currencyexchangeservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
 public class CurrencyExchangeController {
+
+    @GetMapping("/currency-exchange/{from}/to/{to}")
+    public ExchnageValue retreiveExchnageValue(@PathVariable String from, @PathVariable String to){
+        return new ExchnageValue(1000L,from,to, BigDecimal.valueOf(65));
+    }
 }
